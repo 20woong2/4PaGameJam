a879d47f0da341a761710b47f6dccb35066e7c3b
20woong2 <20woong2@gmail.com>
2025-12-17 14:29:48 +0900

    .

---
+++ Shelltiverse/Assets/Script/DataManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static int[,] Multiverse = new int[4, 3]
+    {
+        { 3, 20, 30 },    // 0青
+        { 40, 50, 60 },    // 1青  
+        { 70, 80, 90 },    // 2青
+        {100,110,120 }     // 3青
+    };
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
